06b0b73d2e135e15cc7682c799c4f56623a9b2a3
Dumilde Matos <dmatos@cpiretail.pt>
2023-07-12 01:24:47 +0100

    first commit

---
+++ src/main/java/com/sb3/springsecurityjwtauth/SpringSecurityJwtAuthApplication.java
@@ -0,0 +1,13 @@
+package com.sb3.springsecurityjwtauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJwtAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJwtAuthApplication.class, args);
+	}
+
+}
