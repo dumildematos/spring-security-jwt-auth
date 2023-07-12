f6ccf50f6f0159b0e1290d677444c96cd40f2fe5
Dumilde Matos <dmatos@cpiretail.pt>
2023-07-12 01:51:00 +0100

    create user entify and repository

---
+++ src/main/java/com/sb3/springsecurityjwtauth/user/Role.java
@@ -0,0 +1,6 @@
+package com.sb3.springsecurityjwtauth.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}
---
+++ src/main/java/com/sb3/springsecurityjwtauth/user/User.java
@@ -0,0 +1,66 @@
+package com.sb3.springsecurityjwtauth.user;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+import static jakarta.persistence.EnumType.STRING;
+import static jakarta.persistence.GenerationType.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "_user")
+public class User implements UserDetails {
+
+    @jakarta.persistence.Id
+    @GeneratedValue
+    private Integer Id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+    @Enumerated(STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
