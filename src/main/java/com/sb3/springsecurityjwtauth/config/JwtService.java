8a00bd8d7e3a0578b2980053fc851ede00f171ba
Dumilde Matos <dmatos@cpiretail.pt>
2023-07-13 06:13:02 +0100

    Create entity and jwtService

---
+++ src/main/java/com/sb3/springsecurityjwtauth/config/JwtService.java
@@ -0,0 +1,39 @@
+package com.sb3.springsecurityjwtauth.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+
+    private static final String SECRET_KEY = "Cm07KK3NcQ4MjIzngPYIrfZ1AU6g9Y76O1m7F2UvSryaS1hpL1+pYzLE69jRmk9u";
+
+    public String extractUsername(String token){
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+    
+    private Claims extractAllClaims(String token){
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
